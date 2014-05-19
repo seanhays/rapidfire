8e4e301d540582940f16e0a115a6e13efc4bc8a5
Yuva <yuva@codemancers.com>
2014-05-19 10:23:39 +0530

    while displaying results, convert nil to empty string

--- app/services/rapidfire/question_group_results.rb
+++ app/services/rapidfire/question_group_results.rb
@@ -14,7 +14,9 @@ module Rapidfire
           case question
           when Rapidfire::Questions::Select, Rapidfire::Questions::Radio,
             Rapidfire::Questions::Checkbox
-            answers = question.answers.map(&:answer_text).map { |text| text.split(',') }.flatten
+            answers = question.answers.map(&:answer_text).map do |text|
+              text.to_s.split(Rapidfire.answers_delimiter)
+            end.flatten
             answers.inject(Hash.new(0)) { |total, e| total[e] += 1; total }
 
           when Rapidfire::Questions::Short, Rapidfire::Questions::Date,
