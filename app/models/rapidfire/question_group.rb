92a59a0abae6e84aa6c51aac5b7baf142d2575f8
sheamf <miles_shea@yahoo.com>
2014-10-18 17:01:17 -0600

    add has_many :answer_groups association to QuestionGroup
    for scoping question groups based on user ids stored in answer_groups

--- app/models/rapidfire/question_group.rb
+++ app/models/rapidfire/question_group.rb
@@ -1,6 +1,7 @@
 module Rapidfire
   class QuestionGroup < ActiveRecord::Base
     has_many  :questions
+    has_many :answer_groups
     validates :name, :presence => true
 
     if Rails::VERSION::MAJOR == 3
